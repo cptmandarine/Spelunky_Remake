cbe25a16c1c7c4c43ad32e54e7f29daa53c155e4
cptmandarine <83871735+cptmandarine@users.noreply.github.com>
2025-11-30 19:40:59 +0900

    [FIX] : 프로젝트 변경

---
+++ Spelunky/Spelunky.cpp
@@ -0,0 +1,41 @@
+﻿// Tetris_ToyProject.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "Spelunky.h"
+#include "MainApp.h"
+
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_TETRISTOYPROJECT, szWindowClass, MAX_LOADSTRING);
+
+    int iMsgParam = 0;
+    CMainApp mainApp;
+
+    if (mainApp.Initilize(hInstance, nCmdShow))
+        iMsgParam = mainApp.Run();
+
+    mainApp.Release();
+
+    return iMsgParam;
+}
